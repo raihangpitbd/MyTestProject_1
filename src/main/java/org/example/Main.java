b5d1d65888c0ddaa21f011bd9885ba16b58a7d10
raihangpitbd <raihan.gpit.bd@gamil.com>
2025-11-22 02:19:41 +0600

    init commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.Scanner;
+
+class MyBat {
+    enum MyBatSize {SMALL, MEDIUM, LARGE};
+    MyBatSize size;
+
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+    MyBat myBat = new MyBat();
+
+    myBat.size = MyBat.MyBatSize.SMALL;
+    System.out.println(myBat.size);
+    }
+}
