40429c46a883b16da73e6069e5d876b56edb8be2
raihangpitbd <raihan.gpit.bd@gamil.com>
2025-12-02 12:54:22 +0600

    init commit 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,66 @@
 package org.example;
+import java.util.*;
 
 import java.util.Scanner;
 
-class MyBat {
-    enum MyBatSize {SMALL, MEDIUM, LARGE};
-    MyBatSize size;
+ class Student {
 
+    private String name;
+    private int rollNo;
+    private String section;
 
+    // Constructor
+    Student(String name, int rollNo, String section){
+        this.name = name;
+        this.rollNo = rollNo;
+        this.section = section;
+    }
+
+    // Getters
+    public String getName() { return name; }
+    public int getRollNo() { return rollNo; }
+    public String getSection() { return section; }
+
+    // Setters
+    public void setName(String name) { this.name = name; }
+    public void setRollNo(int rollNo) { this.rollNo = rollNo; }
+    public void setSection(String section) { this.section = section; }
+
+    public void printDetails() {
+        System.out.println(this.name + ", " + this.rollNo + ", " + this.section);
+    }
 }
 
 public class Main {
-    public static void main(String[] args) {
-    MyBat myBat = new MyBat();
 
-    myBat.size = MyBat.MyBatSize.SMALL;
-    System.out.println(myBat.size);///
+    public static void main(String[] args) {
+
+        // Create a list to store students
+        List<Student> list = new ArrayList<>();
+
+        // Add student objects
+        list.add(new Student("Raihan", 1, "A"));
+        list.add(new Student("Hasan", 2, "B"));
+        list.add(new Student("Mim", 3, "C"));
+
+        // Loop and print all student details
+        for (Student s : list) {
+            s.printDetails();
+        }
+
+
+        Student s = new Student("Raihan", 1, "A");
+        // Using getters
+        System.out.println("Name: " + s.getName());
+        System.out.println("Roll: " + s.getRollNo());
+        System.out.println("Section: " + s.getSection());
+
+        // Using setters
+        s.setName("Karim");
+        s.setRollNo(10);
+        s.setSection("B");
+
+        System.out.println("Updated Details:");
+        System.out.println(s.getName() + ", " + s.getRollNo() + ", " + s.getSection());
     }
 }
